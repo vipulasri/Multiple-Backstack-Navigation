189e7381b30998f8c08845e9a239dabbdd89032d
vipulasri <vipulasri.2007@gmail.com>
2020-07-15 09:21:25 +0530

    add: initial commit

---
+++ app/src/main/java/com/vipulasri/multiplebackstacknavigation/ui/title/TitleFragment.kt
@@ -0,0 +1,31 @@
+package com.vipulasri.multiplebackstacknavigation.ui.title
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.vipulasri.multiplebackstacknavigation.R
+
+class TitleFragment : Fragment() {
+
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    return inflater.inflate(R.layout.fragment_notifications, container, false)
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    val textView = view.findViewById<TextView>(R.id.text_title)
+
+
+  }
+
+}
